3a63d802601dc3cf317dc70dab3d6fc4ff9f1536
sweetOlams <baqiyahmide@gmail.com>
2022-07-25 09:33:09 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector("#name").focus();
+
+// var network = document.getElementsByClassName('phone');
+
+// // for (i = 0; i < network.length; i++) {
+//     if (network === '0703' && network === '0803') {
+//         alert('mtn')
+//     }
